7fabcaad35a381dbfa1eafc9442f80dc8951f050
CheesiestMaster <74749672+CheesiestMaster@users.noreply.github.com>
2021-01-19 13:05:16 -0500

    Added a time until Inauguration counter

--- weather.php
+++ weather.php
@@ -46,3 +46,6 @@
           ?>
            </div>
         </div>
+        <div>
+          <iframe src="http://free.timeanddate.com/countdown/i7n3y74t/n198/cf12/cm0/cu5/ct0/cs0/ca0/cr0/ss0/cac000/cpc000/pct/tcfff/fs100/szw320/szh135/tatTime%20left%20to%20Event%20in/tac000/tptTime%20since%20Event%20started%20in/tpc000/mac000/mpc000/iso2021-01-20T11:30:00" allowTransparency="true" frameborder="0" width="181" height="69"></iframe>
+        </div>
